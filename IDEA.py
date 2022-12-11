26386687ea7ea4aa91557b25f59da8f24b675e3b
vlikiii <lik_200@mail.ru>
2022-12-11 17:57:25 +0300

    upd(Дописан алгоритм шифрования IDEA)

--- IDEA.py
+++ IDEA.py
@@ -1,3 +1,6 @@
+from IDEA_operations import _keys_addition_layer, _multiply_addition_layer
+
+
 class IDEA:
     def __init__(self, key: int):
         self._keys: tuple | None = None
@@ -11,3 +14,35 @@ class IDEA:
 
         for i in range(9 * 6):
             sub_keys.append((key >> (112 - 16 * (i % 8))) % 0x10000)
+            if i % 8 == 7:
+                key = ((key << 25) | (key >> 103)) % modulus
+
+        keys = []
+        for i in range(9):
+            round_keys = sub_keys[6 * i: 6 * (i + 1)]
+            keys.append(tuple(round_keys))
+        self._keys = tuple(keys)
+
+    def encrypt(self, plaintext):
+        assert 0 <= plaintext < (1 << 64), 'длина вводимого текста ' \
+                                           'больше допустимого значения'
+
+        x1 = (plaintext >> 48) & 0xFFFF
+        x2 = (plaintext >> 32) & 0xFFFF
+        x3 = (plaintext >> 16) & 0xFFFF
+        x4 = plaintext & 0xFFFF
+
+        for i in range(8):
+            round_keys = self._keys[i]
+
+            y1, y2, y3, y4 = _keys_addition_layer(x1, x2, x3, x4, round_keys)
+            x1, x2, x3, x4 = _multiply_addition_layer(y1, y2, y3, y4, round_keys)
+
+            x2, x3 = x3, x2
+
+            y1, y2, y3, y4 = _keys_addition_layer(x1, x2, x3, x4, self._keys[8])
+
+            cipher_text = hex((y1 << 48) | (y2 << 32) | (y3 << 16) | y4)
+            return cipher_text
+
+
--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-if __name__ == '__main__':
-    print(1 << 128)
+from IDEA import IDEA
 
+
+def main():
+    # key = 0x00000000000000000000000000000000
+    # plain  = 0x8000000000000000
+    # cipher = 0x8001000180008000
+
+    key = 0x2BD6459F82C5B300952C49104881FF48
+    plain = 0xF129A6601EF62A47
+    cipher = 0xEA024714AD5C4D84
+
+    print('key:\t\t', hex(key))
+    print('plaintext:\t', hex(plain))
+
+    crypto_idea = IDEA(key)
+
+    encrypted = crypto_idea.encrypt(plain)
+
+    assert encrypted == cipher
+
+    print('ciphertext\t', hex(cipher))
+
+
+if __name__ == '__main__':
+    main()
