a61f5fbcc461b26216a86ad4c863ba1a434157a2
vlikiii <lik_200@mail.ru>
2022-12-11 17:24:46 +0300

    add(.docx): добавлен файл с курсовой работой. upd(IDEA.py): обновлён основной скрипт файла

--- IDEA.py
+++ IDEA.py
@@ -1,16 +1,13 @@
 class IDEA:
     def __init__(self, key: int):
-        self.keys: tuple | None = None
+        self._keys: tuple | None = None
         self.change_key(key)
 
     def change_key(self, key: int):
-        modulus = 1 << 128
+        modulus = 1 << 128  # 2 ** 128
         assert 0 <= key < modulus
 
         sub_keys = []
 
         for i in range(9 * 6):
-            sub_keys.append()
-
-
-
+            sub_keys.append((key >> (112 - 16 * (i % 8))) % 0x10000)
